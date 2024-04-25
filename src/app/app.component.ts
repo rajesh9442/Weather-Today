6e0a571c4143bf5da9ee9dbf650dabd3909de8cc
RajeshMahendran <rajeshmahendran1999@gmail.com>
2024-04-25 15:00:44 -0500

    Implemented .ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weatherApp';
+
+  searchForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+  
+  ngOnInit() {
+    this.searchForm = this.fb.group({
+      city:[null,Validators.required]
+    })
+  }
+  }
+
+  searchWeather(){
+    console.log(this.searchForm.value);
+
+  }
 }
